6bbbb9eaa663d84202d524f08ca9b76ba5b4d2f2
sohadutt <sohadutt@users.noreply.github.com>
2026-04-01 09:28:57 +0530

    init

---
+++ backend/portfolio_form/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PortfolioFormConfig(AppConfig):
+    name = 'portfolio_form'
---
+++ backend/portfolio_form/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def submitForm(request):
+    if request.method == 'POST':
+        # Process the form data here
+        return HttpResponse('Form submitted successfully!')
+    else:
+        return HttpResponse('Invalid request method.')
