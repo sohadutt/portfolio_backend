e70cdf2c98302182307373e2d34504c1589f3aa0
sohadutt <sohadutt@users.noreply.github.com>
2026-04-21 00:42:48 +0530

    update

--- start.sh
+++ start.sh
@@ -10,6 +10,6 @@ uv run manage.py collectstatic --noinput
 # Use pool=solo to force Celery to run in the main thread without spawning sub-processes.
 # This severely limits throughput (it can only process 1 task at a time), 
 # but it drastically reduces memory consumption.
-celery -A config worker --loglevel=info --pool=solo &
+celery -A config worker --loglevel=info --pool=solo
 
 exec gunicorn config.wsgi:application --bind "0.0.0.0:${PORT:-10000}"
